ac9796ec727b0f6827a30555869f94253c923e59
shahkishal <shahkishal24.8@gmail.com>
2024-07-18 13:18:49 +0530

    last

--- app.py
+++ app.py
@@ -21,6 +21,7 @@ class Users(db.Model):
         return f"{self.email}-{self.password}"
 @app.route('/',methods=['GET' , 'POST'])
 def hello_world():
+    return redirect("/predict")
     if request.method=='POST':
         email = request.form['email']
         password = request.form['password']
@@ -45,9 +46,7 @@ def predict():
             csv_file = r'final2.csv'
             data = pd.read_csv(csv_file)
             data2 = request.json
-            print(data2['password'])
-            if data2['password'] == "":
-                print('---------------------'+data2['password'])
+           
 
             email = int(data2['email'])  # Convert to integer
             password = int(data2['password'])  # Convert to integer
@@ -61,10 +60,18 @@ def predict():
                 
             
             # Convert selected_data to a list of dictionaries (JSON-like format)
-            selected_data_list = selected_data['symbol'].tolist()
-            symbol_string = ' '.join(selected_data_list)
-            # return send_from_directory(app.static_folder, 'index.html', predictionText=symbol_string)
-            return jsonify({'prediction_text': symbol_string})
+            # selected_data_list = selected_data['symbol'].tolist()
+            
+            # #symbol_string = ' '.join(selected_data_list)
+            # # return send_from_directory(app.static_folder, 'index.html', predictionText=symbol_string)
+            # #return jsonify({'prediction_text': selected_data_list})
+            # predictions = [{'prediction_text': symbol} for symbol in selected_data_list]
+            # return jsonify(predictions)
+            selected_data_list = selected_data['company_name'].tolist()
+            website_links = selected_data['website_link'].tolist()  # assuming you have a 'website_link' column in your CSV file
+
+            predictions = [{'prediction_text': symbol, 'website_link': link} for symbol, link in zip(selected_data_list, website_links)]
+            return jsonify(predictions)
 
 
 @app.route('/chat',methods=['GET' , 'POST'])
